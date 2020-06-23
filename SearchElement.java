edc60c8f400f41a11c5f0038ee4e1a576e3380c7
karpagamVT <66828894+karpagamVT@users.noreply.github.com>
2020-06-23 10:38:05 +0530

    Add files via upload

---
+++ SearchElement.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class SearchElement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int searchelement=4;
+		boolean flag=false;
+		int[] a={4};
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]==searchelement)
+			{
+				flag=true;
+			}
+		}
+		System.out.println(flag);
+		
+
+	}
+
+}
